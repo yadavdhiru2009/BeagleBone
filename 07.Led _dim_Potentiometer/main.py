3eeaa98a23df3eb38c28fa953c5128c83f05dccb
yadavdhiru2009 <yadavdhiru2009@gmail.com>
2023-10-06 23:30:12 +0530

    Led Dimming with BeagleBone Black

---
+++ 07.Led _dim_Potentiometer/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+## Author:dharmendra Kumar Yadavimport Adafruit_BBIO.ADC as ADC
+import Adafruit_BBIO.PWM as PWM
+from time import sleep
+LED="P9_14"
+pot="P9_33"
+ADC.setup()
+PWM.start(LED,0,1000)
+while(1):
+        analogRead=ADC.read(pot)
+        dutyCycle=(101)**analogRead -1
+        print dutyCycle
+        PWM.set_duty_cycle(LED,dutyCycle)
+        sleep(.2)
